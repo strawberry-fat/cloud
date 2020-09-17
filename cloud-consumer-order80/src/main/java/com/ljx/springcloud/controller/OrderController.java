1ed2a083ddfcd40fa2779a7400c600962d21c29d
lijinxing <lijinx-pccw@139.com>
2020-09-17 15:30:37 +0800

    RestTemplate的使用

--- cloud-consumer-order80/src/main/java/com/ljx/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/ljx/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.ljx.springcloud.controller;
 import com.ljx.springcloud.entity.CommonResult;
 import com.ljx.springcloud.entity.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -29,13 +30,36 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
     @GetMapping("/payment/add")
-    public CommonResult add(Payment payment){
+    public CommonResult<Payment> add(Payment payment){
         log.info("123");
         log.info("789");
           return restTemplate.postForObject(PAYMENT_URL+"/payment/add",payment,CommonResult.class);
     }
+
+    @PostMapping("/payment/post")
+    public CommonResult<Payment> add2(@RequestBody Payment payment){
+        ResponseEntity<Payment> paymentResponseEntity = restTemplate.postForEntity(PAYMENT_URL + "/payment/add", payment, Payment.class);
+        if(paymentResponseEntity.getStatusCode().is2xxSuccessful()){
+            return new CommonResult<> (200,"添加成功",payment);
+        }else {
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+
     @GetMapping("/payment/get/{id}")
-    public CommonResult getPaymentById(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
+        //返回对象为json，是响应体重数据转换的对象
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+
+    @GetMapping("/payment/getEntity/{id}")
+    public CommonResult<Payment> getPaymentEntityById(@PathVariable("id") Long id){
+        //返回对象为ResponseEntity，包含了响应的一些重要信息。比如响应头，响应状态码，响应体等。
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+                if(entity.getStatusCode().is2xxSuccessful()){
+                    return entity.getBody();
+                }else {
+                    return new CommonResult<>(444,"操作失败");
+                }
     }
 }
