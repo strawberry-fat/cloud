f0b4f21e9f3859d36cacd1b12d206c5693b3a56e
lijinxing <lijinx-pccw@139.com>
2020-11-02 15:25:23 +0800

    CloudConfig客户端的使用，并结合actuator可以手动刷新配置文件

---
+++ cloud-config-center3344/src/main/java/com/ljx/springcloud/CongifCenterMain3344.java
@@ -0,0 +1,13 @@
+package com.ljx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CongifCenterMain3344 {
+    public static void main(String[] args) {
+        SpringApplication.run(CongifCenterMain3344.class);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/ljx/springcloud/controller/ConfigController.java
@@ -0,0 +1,24 @@
+package com.ljx.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @RefreshScope：当配置中心的配置文件更新之后，手动发送一个post请求到
+ *                  localhost:3355/actuator/refresh，
+ *                  可以不重启服务就能重新加载新的配置文件
+ *                  必须有@RefreshScope注解和actuator依赖。
+ */
+@RefreshScope
+@RestController
+public class ConfigController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
